2c89d2a5277554d7aee79137b5c3dff52e86d0c1
Martin Torrey <hockeymt.torrey@gmail.com>
2022-12-12 03:06:18 -0500

    alter/delete base files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
-import Funny from './components/Funny';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Funny/>
     </div>
     
   );
--- src/components/Funny.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Funny = () => {
-  return (
-    <div className='anime-image'></div>
-  )
-}
-
-export default Funny
