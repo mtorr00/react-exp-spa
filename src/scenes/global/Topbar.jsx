e6b578e813dcb33f335a24a41e47a31ea86d21f5
Martin Torrey <hockeymt.torrey@gmail.com>
2022-12-12 09:08:34 -0500

    starting topbar global component

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import { React, useContext} from 'react'
+import { Box, IconButton, useTheme } from "@mui/material"
+import { ColorModeContext, tokens } from "../../theme"
+import InputBase from '@mui/material'
+import { LightModeOutlinedIcon } from "@mui/icons-material/LightModeOutlined"
+import { DarkModeOutlinedIcon } from "@mui/icons-material/DarkModeOutlined"
+import { NotificationsOutlinedIcon } from "@mui/icons-material/NotificationsOutlined"
+import { SettingsOutlinedIcon } from "@mui/icons-maOutlined"
+import { PersonOutlinedIcon } from "@mui/icons-material/PersonOutlined"
+import { SearchIcon } from "@mui/icons-material/Search"
 
 const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  
   return (
     <div>Topbar</div>
   )
